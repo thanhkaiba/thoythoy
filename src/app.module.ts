19d4b45631f7107749185caf4e41377abefc9c42
HeartEarthDragon <traitimtanvo3004@gmail.com>
2022-06-28 22:11:04 +0700

    feat: read database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ProductController } from './product.controller';
+import { ProductController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductService } from './products.service';
+import { Product } from './models/product.entity';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "root",
+      "password": "root",
+      "database": "online_store",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+  }),
+  TypeOrmModule.forFeature([Product])
+  ],
   controllers: [AppController, ProductController],
-  providers: [],
+  providers: [ProductService],
 })
 export class AppModule {}
---
+++ src/products.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Render, Param, Res } from '@nestjs/common';
+import { ProductService } from './products.service';
+
+@Controller('/products')
+export class ProductController {
+
+  constructor(private readonly productService: ProductService) {
+
+  }
+ 
+
+  @Get('/')
+  @Render('products/index')
+  async index() {
+    const viewData = [];
+    viewData['title'] = 'Products-Online Store';
+    viewData['subtitle'] = 'List of products';
+    viewData['products'] = await this.productService.findAll();
+    return {
+      viewData,
+    };
+  }
+
+  @Get('/:id')
+  async show(@Param() params, @Res() response) {
+    const product = await this.productService.findOne(params.id);
+    console.log(product);
+    if (product == null) {
+      return response.redirect('/products');
+    }
+    const viewData = [];
+    viewData['title'] = product.name + '-Online Store';
+    viewData['subtitle'] = product.name + '-Product Information';
+    viewData['product'] = product;
+    return response.render('products/show', {viewData})
+  }
+}
