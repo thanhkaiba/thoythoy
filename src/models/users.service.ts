9c3016aa60a4ef6c3dc23f6b294398bd5f8fdedc
HeartEarthDragon <traitimtanvo3004@gmail.com>
2022-07-06 22:36:59 +0700

    feat: add flow login

--- src/models/users.service.ts
+++ src/models/users.service.ts
@@ -1,20 +1,30 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { User } from "./user.entity";
-import * as bcrypt from "bcrypt";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private usersRepository: Repository<User>) {
-        
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async createOrUpdate(user: User): Promise<User> {
+    const hash = await bcrypt.hash(user.getPassword(), 10);
+    user.setPassword(hash);
+    return this.usersRepository.save(user);
+  }
+
+  async login(email: string, password: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: [{ email }] });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.getPassword());
+      if (isMatch) {
+        return user;
+      }
     }
 
-    async createOrUpdate(user: User): Promise<User> {
-        const hash = await bcrypt.hash(user.getPassword(), 10);
-        user.setPassword(hash);
-        return this.usersRepository.save(user);
-    }
-
-    
+    return null;
+  }
 }
